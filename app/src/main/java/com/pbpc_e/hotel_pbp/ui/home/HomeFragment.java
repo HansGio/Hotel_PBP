6694245eb22cd0f14a1a2a31ece4a28d6a753c12
HansGio <hansgio007@gmail.com>
2020-10-10 21:08:36 +0700

    add navGraph for HomeFragment

--- app/src/main/java/com/pbpc_e/hotel_pbp/ui/home/HomeFragment.java
+++ app/src/main/java/com/pbpc_e/hotel_pbp/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.pbpc_e.hotel_pbp.ui.home;
 
 import android.os.Bundle;
+import android.transition.Slide;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,28 +9,65 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.navigation.NavigationView;
 import com.pbpc_e.hotel_pbp.R;
+import com.pbpc_e.hotel_pbp.ui.about.AboutFragment;
+import com.pbpc_e.hotel_pbp.ui.book.BookFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    ImageSlider imageSlider;
+    CardView cardViewBook, cardViewAbout, cardViewProfile;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        cardViewAbout = root.findViewById(R.id.cardview_about);
+        cardViewBook = root.findViewById(R.id.cardview_book);
+        cardViewProfile = root.findViewById(R.id.cardview_profile);
+
+        cardViewAbout.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_nav_about);
             }
         });
+        cardViewBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_nav_book);
+            }
+        });
+        cardViewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_nav_profile);
+            }
+        });
+
+        imageSlider = root.findViewById(R.id.image_slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel("https://hoteltentrem.com/wp-content/uploads/2020/01/tentrem-hotel-yogyakarta-gallery-pool-jpg.jpg", "", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel("https://hoteltentrem.com/wp-content/uploads/2020/01/tentrem-hotel-yogyakarta-gallery-Room-Deluxe-King.jpg", "", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel("https://hoteltentrem.com/wp-content/uploads/2020/01/art-gallery.jpg", "", ScaleTypes.CENTER_CROP));
+        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
+
         return root;
     }
 }
